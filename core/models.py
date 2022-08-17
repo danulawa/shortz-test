d3bc93333e6f36a98bb7645bec9f78be70c24732
danulawa <danula.wa@gmail.com>
2022-08-17 15:31:20 +0530

    update

--- core/models.py
+++ core/models.py
@@ -4,5 +4,5 @@ from datetime import datetime
 class ShortUrls(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original_url = db.Column(db.String(50000), nullable=False)
-    short_id = db.Column(db.String(200), nullable=False, unique=True)
+    short_id = db.Column(db.String(1000), nullable=False, unique=True)
     created_at = db.Column(db.DateTime(), default=datetime.now(), nullable=False)
