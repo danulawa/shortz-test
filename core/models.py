1b04cd318125e58a779e925e86dc348b73cbe269
danulawa <danula.wa@gmail.com>
2022-12-11 23:33:14 +0530

    ui-update

--- core/models.py
+++ core/models.py
@@ -6,3 +6,9 @@ class ShortUrls(db.Model):
     original_url = db.Column(db.String(50000), nullable=False)
     short_id = db.Column(db.String(1000), nullable=False, unique=True)
     created_at = db.Column(db.DateTime(), default=datetime.now(), nullable=False)
+
+class UserInfo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(10), nullable=False, unique=True)
+    usermail = db.Column(db.String(500), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
--- core/routes.py
+++ core/routes.py
@@ -1,11 +1,19 @@
 from ast import Delete
 from datetime import datetime
-from core.models import ShortUrls
+
+from flask_login import login_required
+from core.models import ShortUrls, UserInfo
 from core import app, db
 from random import choice
 import string
 from flask import render_template, request, flash, redirect, url_for
 import sqlite3 as sql
+
+
+# =====================================================
+# Link Management
+# =====================================================
+
 
 
 def generate_short_id(num_of_chars: int):
@@ -92,3 +100,92 @@ def delete(id):
     all_data = ShortUrls.query.all()
 
     return render_template('index.html', datas=all_data)
+
+
+# =====================================================
+# User Management
+# =====================================================
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username and UserInfo.query.filter_by(username=username).first() is not None:
+            if password and UserInfo.query.filter_by(password=password).first() is not None:
+                
+                return redirect(url_for('index'))
+
+# @app.route('/all-user', methods=['GET', 'POST'])
+# def all_user():
+
+#     if request.method == 'POST':
+#         username = request.form['username']
+#         usermail = request.form['usermail']
+#         password = request.form['password']
+
+#         if username and ShortUrls.query.filter_by(username=username).first() is not None:
+#             flash('Please enter different username!')
+#             return redirect(url_for('index'))
+
+#         if not username:
+#             flash('A username is required!')
+#             return redirect(url_for('index'))
+
+#         if not usermail:
+#             flash('An e-mail is required!')
+#             return redirect(url_for('index'))
+
+#         if not password:
+#             flash('An password is required!')
+#             return redirect(url_for('index'))
+
+
+#         new_user = UserInfo(username=username, usermail=usermail, password=password)
+#         db.session.add(new_user)
+#         db.session.commit()
+
+#         all_users = UserInfo.query.all()
+#         return render_template('users.html', datas=all_users)
+
+#     all_users = ShortUrls.query.all()
+#     return render_template("users.html",datas=all_users)
+
+# @app.route('/update-user/<id>', methods=['GET', 'POST'])
+# def update_user(id):
+
+#     if request.method == 'POST':
+
+#         url = request.form['url']
+#         short_id = request.form['custom_id']
+
+#         UpdatedUrl = UserInfo.query.filter_by(id=id).first()
+
+#         UpdatedUrl.id = id
+#         UpdatedUrl.original_url = url
+#         UpdatedUrl.short_id = short_id
+        
+#         db.session.commit()
+#         short_url = request.host_url + short_id
+
+#         all_data = UserInfo.query.all()
+
+#         return render_template('index.html', datas=all_data)
+    
+#     row_data = UserInfo.query.filter_by(id=id).first()
+#     return render_template('update.html',datas=row_data)
+
+
+# @app.route('/delete-user/<id>', methods=['GET', 'POST'])
+# def delete_user(id):
+
+#     DeleteRow = UserInfo.query.filter_by(id=id).first()
+
+#     db.session.delete(DeleteRow)       
+#     db.session.commit()
+
+#     all_users = UserInfo.query.all()
+
+#     return render_template('index.html', datas=all_users)
